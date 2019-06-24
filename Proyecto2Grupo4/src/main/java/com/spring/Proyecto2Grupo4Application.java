076400ddca6fc94e3a91240c75db3a9592515166
Edmund Home <edmundhome@hotmail.com>
2019-06-24 16:35:57 +0200

    proyecto creado

---
+++ Proyecto2Grupo4/src/main/java/com/spring/Proyecto2Grupo4Application.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto2Grupo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto2Grupo4Application.class, args);
+	}
+
+}
