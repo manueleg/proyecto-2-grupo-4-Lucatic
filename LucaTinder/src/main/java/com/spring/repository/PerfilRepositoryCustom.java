7258659abb3129a39a6fde2f654e794309f1aaac
Edmund Home <edmundhome@hotmail.com>
2019-06-27 19:34:22 +0200

    ḿétodo like en PerfilRepositoryCustom y PerfilRepositoryImpl

--- LucaTinder/src/main/java/com/spring/repository/PerfilRepositoryCustom.java
+++ LucaTinder/src/main/java/com/spring/repository/PerfilRepositoryCustom.java
@@ -5,4 +5,5 @@ import com.spring.model.Perfil;
 
 public interface PerfilRepositoryCustom {
 	public List<Perfil> getPerfiles();
+	public void like(int id1, int id2);
 }
--- LucaTinder/src/main/java/com/spring/repository/PerfilRepositoryImp.java
+++ LucaTinder/src/main/java/com/spring/repository/PerfilRepositoryImp.java
@@ -6,8 +6,13 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.spring.model.Perfil;
 
+@Repository
+@Transactional(readOnly = true)
 public class PerfilRepositoryImp implements PerfilRepositoryCustom{
 
 	@PersistenceContext 
@@ -20,4 +25,19 @@ public class PerfilRepositoryImp implements PerfilRepositoryCustom{
 		return query.getResultList();
 	}
 
+	/**
+	 * Método like de tipo void que recoge dos parámetros id de tipo int y los guardará en la tabla contactos
+	 * de la base de datos.
+	 * @param int id1, int id2
+	 */
+	@Override
+	public void like(int id1, int id2) {
+		// TODO Auto-generated method stub
+		entityManager.createNativeQuery("INSERT INTO lucatinder.contactos (idcontacto, fk_idusuario, fk_idusuario2) VALUES (?,?,?)")
+	      .setParameter(1, null)
+	      .setParameter(2, id1)
+	      .setParameter(3, id2)
+	      .executeUpdate();
+	}
+	
 }
