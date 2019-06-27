43da1c4ce113aa7178541eb976afddf9f1c7adbd
Daniel Moreno <danimj1989@gmail.com>
2019-06-27 15:51:32 +0200

    sdfg

---
+++ LucaTinder/src/main/java/com/spring/services/PerfilServices.java
@@ -0,0 +1,27 @@
+package com.spring.services;
+
+import java.util.List;
+
+import com.spring.model.User;
+
+/**
+ * 
+ * @author David Nombre de clase: PerfilServices Descripcion: Metodos 
+ * para añadir, actualizar y borrar perfil Fecha: 27/06/2019
+ * @version: v1.0
+ *
+ */
+
+
+
+public interface PerfilServices {
+
+	public List<Perfil> list();
+	
+	public Perfil get(int id);
+	
+	public void update(Perfil user);
+	public void add(Perfil user);
+	
+	public void delete(int id);
+}
