93d7229086c3cac966fc18a4d1d41586f38ac626
Edmund Home <edmundhome@hotmail.com>
2019-07-03 12:06:58 +0200

    .

--- Cliente-Lucatinder/src/app/app.module.ts
+++ Cliente-Lucatinder/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PerfilMatchComponent } from './components/perfil-match/perfil-match.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    PerfilMatchComponent,
 
 
 
