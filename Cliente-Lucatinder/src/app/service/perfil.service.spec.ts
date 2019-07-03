62383aae95f7f59bb32c0d2b62c3874a16a34826
cristina <edmundhome@hotmail.com>
2019-07-03 15:05:01 +0200

    .

--- Cliente-Lucatinder/src/app/service/perfil.service.spec.ts
+++ Cliente-Lucatinder/src/app/service/perfil.service.spec.ts
@@ -1,12 +1,8 @@
-import { TestBed } from '@angular/core/testing';
+import {  Perfil } from '../models/perfil';
 
-import { PerfilService } from './perfil.service';
-
-describe('PerfilService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
-
-  it('should be created', () => {
-    const service: PerfilService = TestBed.get(PerfilService);
-    expect(service).toBeTruthy();
+describe('Perfil', () => {
+  it('should create an instance', () => {
+    expect(new Perfil()).toBeTruthy();
   });
 });
+
--- Cliente-Lucatinder/src/app/service/perfil.service.ts
+++ Cliente-Lucatinder/src/app/service/perfil.service.ts
@@ -3,10 +3,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Perfil } from '../models/perfil';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 @Injectable({
   providedIn: 'root'
 })
 export class PerfilService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  private perfilUrl = 'http://localhost:8080/indexrest';
 }
